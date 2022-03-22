0a85bb96707c2af489f3d354ef1c2b89fb03a822
Jerin Lee <taurenjerin@qq.com>
2022-03-22 19:35:23 +0800

    feat: + DID context.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

--- dist/src/id/DASContext.d.ts
+++ dist/src/id/DASContext.d.ts
@@ -1,2 +1,9 @@
+import Das from "das-sdk";
 export declare class DASContext {
+    das: Das;
+    DID: string;
+    AvatarUri: string;
+    constructor();
+    getDID(address: string, coin_type?: string): Promise<string>;
+    process(address: string): Promise<void>;
 }
--- src/id/DASContext.ts
+++ src/id/DASContext.ts
@@ -1,4 +1,41 @@
+import Das, { AccountInfo } from "das-sdk";
+import { Settings } from "../Settings";
 
-export class DASContext{
-    
+export class DASContext {
+    das: Das;
+
+    DID: string = "";
+    AvatarUri: string = "";
+    //DIDAccount: AccountInfo;
+    constructor() {
+        this.das = new Das({
+            url: Settings.RPCEndpoint_DAS,
+        });
+    }
+
+    async getDID(address: string, coin_type: string = "60") {
+        const did = await this.das.reverseRecord({
+            type: 'blockchain',
+            key_info: {
+                coin_type: coin_type,
+                key: address
+            }
+        });
+        return did;
+    }
+
+    async process(address: string){
+        // // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+        this.DID = await this.getDID(address, "60"); // ETH
+        if(!this.DID)
+            this.DID = await this.getDID(address, "714"); // BSC
+        if(!this.DID)
+            this.DID = await this.getDID(address, "966"); // MATIC
+        if(!this.DID)
+            this.DID = await this.getDID(address, "195"); // TRX
+        
+        let account = await this.das.account(this.DID);
+        this.AvatarUri = account.avatar;
+    }
+
 }
